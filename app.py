9b3abc8b295e1b8b722c8f469fcd11cbee672b16
vicky <vigneshveera090@gmail.com>
2020-07-11 17:51:24 +0530

    newupdate

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from MySQLdb.cursors import Cursor
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -31,11 +32,11 @@ def index():
         c.intersection_update(d)
         g = len(c)
         re = get_result(g)
-        cur= mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         this_query = "insert into logs (name_one,name_two,result) values ('" + first_name + "','" + last_name + "','" + re + "')"
-        que="select * from logs"
+        que = "select * from logs"
         cur.execute(que)
-        table=cur.fetchall()
+        table = cur.fetchall()
         cur.execute(this_query)
         mysql.connection.commit()
         cur.close()
@@ -47,15 +48,15 @@ def get_result(g):
     if g in [5, 14, 16, 18, 21, 23]:
         return "FRIENDS"
     elif g in [3, 10, 19]:
-        return "LOVER"
+        return "LOVERS"
     elif g in [8, 12, 13, 17]:
-        return "Affection"
+        return "AFFECTION"
     elif g in [6, 11, 15, 26]:
         return "MARRIAGE"
     elif g in [2, 4, 7, 9, 20, 22, 24, 25]:
         return "ENEMY"
     else:
-        return "SIBLINGS"
+        return "SISTERS"
 
 
 # login_page
@@ -66,7 +67,8 @@ def login():
         user_name = details['user_name']
         password = details['password']
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cur.execute("SELECT * FROM login_table WHERE user_name = '"+user_name+"'  AND password = '"+password+"' ")
+        cur.execute(
+            "SELECT * FROM login_table WHERE user_name = '" + user_name + "'  AND password = '" + password + "' ")
         # Fetch one record and return result
         account = cur.fetchone()
         # If account exists in accounts table in out database
@@ -84,35 +86,37 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/newlogin",methods=["POST","GET"])
+@app.route("/newlogin", methods=["POST", "GET"])
 def newlogin():
     if request.method == "POST":
         details = request.form
         user_name = details['user_name']
         password = details['password']
-        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        this_query = "insert into login_table (user_name,password) values (%s,%s)"
-        cur.execute(this_query,[user_name,password])
-        mysql.connection.commit()
-        cur.close()
-        return redirect('login')
+        if (user_name and password) != " ":
+            cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            this_query = "insert into login_table (user_name,password) values (%s,%s)"
+            cur.execute(this_query, [user_name, password])
+            mysql.connection.commit()
+            cur.close()
+            return redirect('login')
+        
 
     return render_template("newlogin.html")
 
 
-@app.route('/admin',methods=["POST","GET"])
+@app.route('/admin', methods=["POST", "GET"])
 def admin():
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("SELECT * FROM logs")
     # Fetch one record and return result
     info = cur.fetchall()
     if session.get('logged_in'):
-        return render_template("admin.html",tables=info)
+        return render_template("admin.html", tables=info)
     else:
         return redirect(url_for('login'))
 
 
-@app.route('/edit/<string:id>',methods=["POST","GET"])
+@app.route('/edit/<string:id>', methods=["POST", "GET"])
 def edit(id):
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     if request.method == "POST":
@@ -121,28 +125,36 @@ def edit(id):
         last_name = details['couple_name']
         re = details['result']
         this_query = "update logs set name_one=%s,name_two=%s,result=%s where log_id=%s"
-        cur.execute(this_query,[first_name,last_name,re,id])
+        cur.execute(this_query, [first_name, last_name, re, id])
         mysql.connection.commit()
         cur.close()
         return redirect(url_for("admin"))
         cur = mysql.connection.cursor()
 
-    qeu="select * from logs where log_id=%s"
-    cur.execute(qeu,[id])
-    res=cur.fetchone()
-    return render_template("edit.html",datas=res)
+    qeu = "select * from logs where log_id=%s"
+    cur.execute(qeu, [id])
+    res = cur.fetchone()
+    return render_template("edit.html", datas=res)
 
-@app.route('/delete/<string:id>',methods=["POST","GET"])
+
+@app.route('/delete/<string:id>', methods=["POST", "GET"])
 def delete(id):
     cur = mysql.connection.cursor()
-    sql="delete from logs where log_id=%s"
-    cur.execute(sql,[id])
+    sql = "delete from logs where log_id=%s"
+    cur.execute(sql, [id])
     mysql.connection.commit()
     cur.close()
     return redirect(url_for("admin"))
 
 
+@app.route("/log_out")
+def log_out():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('flames'))
+
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+
+    app.run(debug = True)
